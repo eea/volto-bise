ca6aa9295d405362e716c99fde41a2de637d537b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-07-28 16:41:39 +0300

    Added files from bise-frontend package

---
+++ razzle.extend.js
@@ -0,0 +1,15 @@
+const plugins = (defaultPlugins) => {
+  return defaultPlugins.concat(['scss']);
+};
+const modify = (config, { target, dev }, webpack) => {
+  config.resolve.alias[
+    '../../theme.config$'
+  ] = `${__dirname}/theme/theme.config`;
+
+  return config;
+};
+
+module.exports = {
+  plugins,
+  modify,
+};
