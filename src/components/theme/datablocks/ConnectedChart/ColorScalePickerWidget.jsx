c0eb7e0eb112d1ed8833e08bcc617b88c9aa4aed
Silviu Bogan <silviubogan@gmail.com>
2020-09-09 13:23:21 +0300

    New ColorScalePickerWidget
    There still are some issues with its style.

--- src/components/theme/datablocks/ConnectedChart/ColorScalePickerWidget.jsx
+++ src/components/theme/datablocks/ConnectedChart/ColorScalePickerWidget.jsx
@@ -7,16 +7,21 @@ import { FormFieldWrapper } from '@plone/volto/components';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Loadable from 'react-loadable';
-// import { Input } from 'semantic-ui-react';
-// import { ColorscalePicker } from 'react-colorscales';
+import { Dropdown, Button } from 'semantic-ui-react';
+import './style.less';
 
 const LoadableColorscalePicker = Loadable({
   loader: () => import('react-colorscales'),
-  loading: () => <div>Loading...</div>,
-  // render(loaded, props) {
-  //   let Component = loaded.ColorscalePicker;
-  //   return <Component {...props} />;
-  // },
+  loading: () => <></>,
+});
+
+const LoadableColorscale = Loadable({
+  loader: () => import('react-colorscales'),
+  loading: () => <></>,
+  render: (loaded, props) => {
+    const Component = loaded.Colorscale;
+    return <Component {...props} />;
+  },
 });
 
 /**
@@ -35,30 +40,54 @@ const ColorScalePickerWidget = ({
   onClick,
   fieldSet,
   wrapped,
-  minLength,
-  maxLength,
-}) => (
-  <FormFieldWrapper
-    id={id}
-    title={title}
-    description={description}
-    required={required}
-    error={error}
-    fieldSet={fieldSet}
-    wrapped={wrapped}
-  >
-    {__CLIENT__ && (
-      <LoadableColorscalePicker
-        id={`field-${id}`}
-        name={id}
-        colorscale={value}
-        onChange={(colorScale) => {
-          onChange(id, colorScale);
+}) => {
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+  const [selectedColorScale, setSelectedColorScale] = React.useState(value);
+
+  return (
+    <FormFieldWrapper
+      id={id}
+      title={title}
+      description={description}
+      required={required}
+      error={error}
+      fieldSet={fieldSet}
+      wrapped={wrapped}
+      className={`color-scale-picker-widget`}
+    >
+      <Dropdown
+        open={dropdownOpen}
+        onClose={() => {
+          setDropdownOpen(false);
         }}
-      />
-    )}
-  </FormFieldWrapper>
-);
+        trigger={
+          <LoadableColorscale
+            colorscale={selectedColorScale}
+            onClick={() => {
+              setDropdownOpen(!dropdownOpen);
+            }}
+          />
+        }
+      >
+        <Dropdown.Menu>
+          {__CLIENT__ && (
+            <LoadableColorscalePicker
+              id={`field-${id}`}
+              width={20} // not working
+              scaleLength={5}
+              name={id}
+              colorscale={value}
+              onChange={(colorScale) => {
+                setSelectedColorScale(colorScale);
+                onChange(id, colorScale);
+              }}
+            />
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+    </FormFieldWrapper>
+  );
+};
 
 /**
  * Property types.
@@ -75,8 +104,6 @@ ColorScalePickerWidget.propTypes = {
   onChange: PropTypes.func.isRequired,
   onBlur: PropTypes.func,
   onClick: PropTypes.func,
-  minLength: PropTypes.number,
-  maxLength: PropTypes.number,
   wrapped: PropTypes.bool,
 };
 
@@ -89,12 +116,21 @@ ColorScalePickerWidget.defaultProps = {
   description: null,
   required: false,
   error: [],
-  value: [],
+  value: [
+    'rgb(166,206,227)',
+    'rgb(31,120,180)',
+    'rgb(178,223,138)',
+    'rgb(51,160,44)',
+    'rgb(251,154,153)',
+    'rgb(227,26,28)',
+    'rgb(160,26,51)',
+    'rgb(227,178,223)',
+    'rgb(31,227,28)',
+    'rgb(227,120,180)',
+  ],
   onChange: () => {},
   onBlur: () => {},
   onClick: () => {},
-  minLength: null,
-  maxLength: null,
 };
 
 export default ColorScalePickerWidget;
