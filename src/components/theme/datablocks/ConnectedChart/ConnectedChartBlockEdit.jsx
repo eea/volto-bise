a3e4c177b65259e685a72c64512b6216c5bb2320
Silviu Bogan <silviubogan@gmail.com>
2020-09-10 14:21:27 +0300

    Modularized code

--- src/components/theme/datablocks/ConnectedChart/ConnectedChartBlockEdit.jsx
+++ src/components/theme/datablocks/ConnectedChart/ConnectedChartBlockEdit.jsx
@@ -26,110 +26,135 @@ class Edit extends Component {
     this.color_fields = {};
   }
 
-  getSchema = (chartData, schema) => {
-    if (chartData.data.length > 0 && chartData.data[0].type === 'bar') {
-      let usedSchema = JSON.parse(JSON.stringify(schema || {}));
+  handleFieldChange = (chartData, id, value) => {
+    // some x color changed by user
+    const isAxisX = id.startsWith('x_');
+    // some y color changed by user
+    const isAxisY = id.startsWith('y_');
 
-      usedSchema.properties.categorical_axis = {
-        widget: 'flexible_choices',
-        title: 'Categorical axis',
-        choices: [
-          ['x', 'X'],
-          ['y', 'Y'],
-        ],
-        hasNoValueItem: true,
-        default:
-          this.props.data.categorical_axis === 'x'
-            ? 0
-            : this.props.data.categorical_axis === 'y'
-            ? 1
-            : 2,
-      };
-      usedSchema.fieldsets[usedSchema.fieldsets.length - 1].fields.push(
-        'categorical_axis',
-      );
+    // if some x or y color changed by user
+    if (isAxisX || isAxisY) {
+      this.color_fields[id] = value;
 
-      if (!this.props.data.categorical_axis) {
-        return usedSchema;
-      }
-
-      usedSchema.properties.categorical_colorscale = {
-        title: 'Categorical color scale',
-        type: 'colorscale',
-        default: this.props.data.categorical_colorscale,
-      };
-      usedSchema.fieldsets[usedSchema.fieldsets.length - 1].fields.push(
-        'categorical_colorscale',
-      );
-
-      const axis = this.props.data.categorical_axis;
-
-      // for each unique value on the X axis
+      const axis = isAxisX ? 'x' : 'y';
+      // take all the given axis values
       const xValues = chartData.data[0][axis];
-      const xNoDupl = _.uniq(xValues);
+      // update the colors on that axis
+      // console.log('PROPS', this.props.data);
+      this.props.onChangeBlock(this.props.block, {
+        ...this.props.data,
+        bar_colors: this.getBarColors(axis, xValues, this.color_fields),
+      });
+    } else if (
+      chartData.data &&
+      typeof chartData.data[0] !== 'undefined' &&
+      chartData.data[0].type !== 'bar'
+    ) {
+      this.color_fields = {};
+      const obj = { ...this.props.data };
+      delete obj.categorical_axis;
+      delete obj.categorical_colorscale;
+      delete obj.bar_colors;
+      this.props.onChangeBlock(this.props.block, obj);
+    } else {
+      this.props.onChangeBlock(this.props.block, {
+        ...this.props.data,
+        [id]: value,
+      });
+    }
+  };
 
-      // for each color in the selected colorscale, create an option for it
-      const choices = (
-        this.props.data.categorical_colorscale || biseColorscale
-      ).map((x, i) => [i + 1, `Colour #${i + 1}`]);
+  alterSchemaForBarChart(chartData, schema) {
+    let usedSchema = JSON.parse(JSON.stringify(schema || {}));
 
-      let idx = 1;
-      // for each non-duplicate value
-      for (const val of xNoDupl) {
-        // create a field for it for the end-user
-        const id = this.props.data.categorical_axis + '_' + idx;
+    usedSchema.properties.categorical_axis = {
+      widget: 'flexible_choices',
+      title: 'Categorical axis',
+      choices: [
+        ['x', 'X'],
+        ['y', 'Y'],
+      ],
+      hasNoValueItem: true,
+      default:
+        this.props.data.categorical_axis === 'x'
+          ? 0
+          : this.props.data.categorical_axis === 'y'
+          ? 1
+          : 2,
+    };
+    usedSchema.fieldsets[usedSchema.fieldsets.length - 1].fields.push(
+      'categorical_axis',
+    );
 
-        // console.log('bar_colors', this.props.data.bar_colors);
-        // console.log('xValues and val', { xValues, val });
-
-        // a color is already set for it
-        let index = this.props.data.bar_colors?.[
-          xValues.findIndex((x) => x === val)
-        ];
-
-        usedSchema.properties[id] = {
-          widget: 'flexible_choices',
-          title: val.toString(),
-          hasNoValueItem: false,
-          choices,
-          default:
-            typeof index === 'number'
-              ? index - 1
-              : idx <= choices.length
-              ? idx - 1
-              : 0,
-        };
-        usedSchema.fieldsets[usedSchema.fieldsets.length - 1].fields.push(id);
-
-        ++idx;
-      }
-
-      // console.log('props', this.props);
-
+    if (!this.props.data.categorical_axis) {
       return usedSchema;
+    }
+
+    usedSchema.properties.categorical_colorscale = {
+      title: 'Categorical color scale',
+      type: 'colorscale',
+      default: this.props.data.categorical_colorscale,
+    };
+    usedSchema.fieldsets[usedSchema.fieldsets.length - 1].fields.push(
+      'categorical_colorscale',
+    );
+
+    const axis = this.props.data.categorical_axis;
+
+    // for each unique value on the X axis
+    const xValues = chartData.data[0][axis];
+    const xNoDupl = _.uniq(xValues);
+
+    // for each color in the selected colorscale, create an option for it
+    const choices = (
+      this.props.data.categorical_colorscale || biseColorscale
+    ).map((x, i) => [i + 1, `Colour #${i + 1}`]);
+
+    let idx = 1;
+    // for each non-duplicate value
+    for (const val of xNoDupl) {
+      // create a field for it for the end-user
+      const id = this.props.data.categorical_axis + '_' + idx;
+
+      // console.log('bar_colors', this.props.data.bar_colors);
+      // console.log('xValues and val', { xValues, val });
+
+      // a color is already set for it
+      let index = this.props.data.bar_colors?.[
+        xValues.findIndex((x) => x === val)
+      ];
+
+      usedSchema.properties[id] = {
+        widget: 'flexible_choices',
+        title: val.toString(),
+        hasNoValueItem: false,
+        choices,
+        default:
+          typeof index === 'number'
+            ? index - 1
+            : idx <= choices.length
+            ? idx - 1
+            : 0,
+      };
+      usedSchema.fieldsets[usedSchema.fieldsets.length - 1].fields.push(id);
+
+      ++idx;
+    }
+
+    return usedSchema;
+  }
+
+  hasBarChart(chartData) {
+    return chartData.data.length > 0 && chartData.data[0].type === 'bar';
+  }
+
+  getSchema = (chartData, schema) => {
+    if (this.hasBarChart(chartData)) {
+      return this.alterSchemaForBarChart(chartData, schema);
     }
 
     return schema;
   };
-
-  // // computes bar colors based on values of the given axis and the value that changed (id, value)
-  // getBarColorsAfterChange = (xValues, id, value) => {
-  //   const xNoDupl = _.uniq(xValues);
-
-  //   // index from the array of uniques
-  //   let idx = Number(id.substring(2)) - 1;
-
-  //   const bar_colors = this.props.data.bar_colors || [];
-
-  //   // for each index in xValues where it is xNoDupl[idx]
-  //   // put it in bar_colors there
-  //   for (let i = 0; i < xValues.length; ++i) {
-  //     if (xValues[i] === xNoDupl[idx]) {
-  //       bar_colors[i] = value;
-  //     }
-  //   }
-  //   return bar_colors;
-  // };
 
   /**
    * @param {string} axis 'x' or 'y'
@@ -155,7 +180,7 @@ class Edit extends Component {
       }
     }
 
-    console.log('OUTPUT getBarColors =', arr);
+    // console.log('OUTPUT getBarColors =', arr);
 
     return arr;
   };
@@ -186,47 +211,9 @@ class Edit extends Component {
           <InlineForm
             schema={usedSchema}
             title={usedSchema.title}
-            onChangeField={(id, value) => {
-              // some x color changed by user
-              const isAxisX = id.startsWith('x_');
-              // some y color changed by user
-              const isAxisY = id.startsWith('y_');
-
-              // if some x or y color changed by user
-              if (isAxisX || isAxisY) {
-                this.color_fields[id] = value;
-
-                const axis = isAxisX ? 'x' : 'y';
-                // take all the given axis values
-                const xValues = chartData.data[0][axis];
-                // update the colors on that axis
-                // console.log('PROPS', this.props.data);
-                this.props.onChangeBlock(this.props.block, {
-                  ...this.props.data,
-                  bar_colors: this.getBarColors(
-                    axis,
-                    xValues,
-                    this.color_fields,
-                  ),
-                });
-              } else if (
-                chartData.data &&
-                typeof chartData.data[0] !== 'undefined' &&
-                chartData.data[0].type !== 'bar'
-              ) {
-                this.color_fields = {};
-                const obj = { ...this.props.data };
-                delete obj.categorical_axis;
-                delete obj.categorical_colorscale;
-                delete obj.bar_colors;
-                this.props.onChangeBlock(this.props.block, obj);
-              } else {
-                this.props.onChangeBlock(this.props.block, {
-                  ...this.props.data,
-                  [id]: value,
-                });
-              }
-            }}
+            onChangeField={(id, value) =>
+              this.handleFieldChange(chartData, id, value)
+            }
             formData={this.props.data}
           />
         </SidebarPortal>
