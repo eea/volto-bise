0845fd0340310b298c7a0a3b62ffcd6986e105d0
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-09-17 00:33:20 +0300

    Improve webmap implementation

--- src/components/Blocks/ConnectedMap/ConnectedMapView.jsx
+++ src/components/Blocks/ConnectedMap/ConnectedMapView.jsx
@@ -3,12 +3,12 @@ import { Style } from '@plone/volto/components';
 import { PrivacyProtection } from 'volto-embed';
 import Webmap from './Webmap';
 
-export default ({ data = {} }) => {
+export default ({ data = {}, ...rest }) => {
   // console.log('data', data);
   return (
     <Style data={data}>
       <PrivacyProtection data={data}>
-        <Webmap {...data} />
+        <Webmap {...rest} data={data} />
       </PrivacyProtection>
     </Style>
   );
--- src/components/Blocks/ConnectedMap/Webmap.jsx
+++ src/components/Blocks/ConnectedMap/Webmap.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { loadModules } from 'esri-loader';
+import './styles.css';
 
 const MODULES = [
   'esri/Map',
   'esri/views/MapView',
-  'esri/widgets/Home',
-  'esri/layers/MapImageLayer',
-  'esri/geometry/Extent',
-  'esri/views/layers/support/FeatureFilter',
   'esri/layers/FeatureLayer',
+  // 'esri/widgets/Home',
+  // 'esri/layers/MapImageLayer',
+  // 'esri/geometry/Extent',
+  // 'esri/views/layers/support/FeatureFilter',
 ];
 
 export default (props) => {
+  const { data } = props;
   const options = {
     css: true,
   };
+  console.log('webmap props', props);
+  const mapRef = React.useRef();
   const [modules, setModules] = React.useState({});
   const loaded = React.useRef(false);
 
@@ -25,25 +29,79 @@ export default (props) => {
         const [
           Map,
           MapView,
-          Home,
-          MapImageLayer,
-          Extent,
-          FeatureFilter,
           FeatureLayer,
+          // Home,
+          // MapImageLayer,
+          // Extent,
+          // FeatureFilter,
         ] = modules;
         setModules({
           Map,
           MapView,
-          Home,
-          MapImageLayer,
-          Extent,
-          FeatureFilter,
           FeatureLayer,
+          // Home,
+          // MapImageLayer,
+          // Extent,
+          // FeatureFilter,
         });
       });
     }
-  }, [modules, setModules, options]);
+  }, [setModules, options]);
 
-  // console.log(modules);
-  return <div>Webmap</div>;
+  const layer_url = `${data.map_service_url}/${data.layer}`;
+  const { base_layer } = data;
+
+  console.log(props.data);
+
+  React.useEffect(() => {
+    const { Map, MapView, FeatureLayer } = modules;
+    if (!FeatureLayer) return;
+
+    let layer = new FeatureLayer({
+      url: layer_url,
+    });
+    const map = new Map({
+      basemap: base_layer || 'hybrid',
+      layers: [layer],
+    });
+    const view = new MapView({
+      container: mapRef.current,
+      map,
+    });
+    view.when(() => {
+      console.log('yes');
+    });
+  }, [modules, layer_url, base_layer]);
+
+  // console.log('fl', FeatureLayer);
+  // let layer = new FeatureLayer({
+  //   url: window.layerUrl,
+  // });
+  // let filter = new FeatureFilter({
+  //   where: "Country = '" + window.focusCountry + "'",
+  // });
+  //
+  // const map = new Map({
+  //   basemap: window.focusMapLayer || 'hybrid', //"gray-vector",
+  //   layers: [layer],
+  // });
+  //
+  // var view = new MapView({
+  //   map: map,
+  //   container: 'viewDiv',
+  //   // extent: focusExtent,
+  // });
+  // view.filter = {
+  //   where: "COUNTRY = '" + window.focusCountry + "'",
+  // };
+  //
+  // view.whenLayerView(layer).then(function (layerView) {
+  //   console.log('whenLayerView', layerView);
+  //
+  //   layerView.filter = {
+  //     where: "Country = '" + window.focusCountry + "'",
+  //   };
+  // });
+
+  return <div ref={mapRef} className="esri-map"></div>;
 };
