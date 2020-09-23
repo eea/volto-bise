29ff62adcbb0cae13f251d34fc160ee22d521ed8
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-09-23 07:58:03 +0300

    Make the connectBlockToProviderData safer

--- src/hocs/connectBlockToProviderData.js
+++ src/hocs/connectBlockToProviderData.js
@@ -6,14 +6,17 @@ export function connectBlockToProviderData(WrappedComponent) {
   return (props) => {
     const dispatch = useDispatch();
 
+    const { data = {} } = props;
     // the URL is always available
-    const { provider_url } = props.data;
+    const { provider_url = null } = data;
 
     // true or false, whether the data is being loaded or not (if it is not
     // loaded it is false, if it has been loading but it finished/stopped, it is
     // false)
     const isPending = useSelector((state) => {
       // the URL for getting whether the provider URL is pending is this (verified):
+      if (provider_url === null) return false;
+
       const url = `${provider_url}`;
       const rv = provider_url
         ? state.data_providers?.pendingConnectors?.[url]
@@ -24,6 +27,8 @@ export function connectBlockToProviderData(WrappedComponent) {
     // this is the actual data, it is loaded well but multiple times
     const provider_data = useSelector((state) => {
       // the URL for getting the data is verified to be this:
+      if (provider_url === null) return null;
+
       const url = `${provider_url}/@connector-data`;
       return provider_url ? state.data_providers?.data?.[url] : null;
     });
