f729c55cd0152d0275dfd520ab1dfa48cf345a25
Silviu Bogan <silviubogan@gmail.com>
2020-09-29 17:37:18 +0300

    Small changes + started using Promises

--- src/components/Blocks/DataCatalogue/DataCatalogueView.jsx
+++ src/components/Blocks/DataCatalogue/DataCatalogueView.jsx
@@ -122,7 +122,7 @@ const RefinementList = class extends SearchkitComponent {
 
     // this.dataTypeInfo = this.getSortedFilteredDataTypeInfo();
     // this.allKeys = this.dataTypeInfo.map((x) => x.key);
-    let selectedKeys = []; // this.selectedItemsFilter(this.allKeys);
+    let selectedKeys = null; // this.selectedItemsFilter(this.allKeys);
     this.state = {
       selectedKeys,
     };
@@ -144,7 +144,7 @@ const RefinementList = class extends SearchkitComponent {
   resetDataTypeFilter = (shouldPerformSearch = true) => {
     const sk = this.searchkit;
 
-    if (!sk) {
+    if (typeof sk === 'undefined') {
       return false;
     }
 
@@ -155,7 +155,7 @@ const RefinementList = class extends SearchkitComponent {
     }
 
     if (shouldPerformSearch) {
-      sk.performSearch(true, true);
+      return sk.performSearch(true, true);
     }
 
     return true;
@@ -174,8 +174,11 @@ const RefinementList = class extends SearchkitComponent {
       const newAccessor = new FacetAccessor('type', {
         operator: 'OR',
         field: '_type',
-        size: 100,
+        size: 10,
         id: 'type',
+        title: 'By Type',
+        // itemComponent={RefinementOption}
+        // listComponent={RefinementList
       });
       sk.addAccessor(newAccessor);
       filters.push(newAccessor);
@@ -186,35 +189,46 @@ const RefinementList = class extends SearchkitComponent {
       x.state = x.state.toggle(key);
     }
 
-    this.refreshSelectedKeys(false);
-
-    sk.performSearch(true, true);
+    console.log('L1', this.props.selectedItems);
+    this.refreshSelectedKeys(false).then(() => {
+      console.log('L2', this.props.selectedItems);
+      sk.performSearch(true, true).then(() => {
+        console.log('L3', this.props.selectedItems);
+      });
+    });
 
     return true;
   };
 
   refreshSelectedKeys = (shouldPerformSearch = true) => {
-    // debugger;
-    // this transforsm [... full ...] into []
-    const initialSelectedCount = this.props.selectedItems.length;
-    let selectedKeys = this.selectedItemsFilter(this.allKeys);
+    return new Promise((resolve, reject) => {
+      // this transforms [... full ...] into []
+      const initialSelectedCount = this.props.selectedItems.length;
+      let selectedKeys = this.selectedItemsFilter(this.allKeys);
 
-    const initialSelectedKeys = this.state.selectedKeys;
+      const initialSelectedKeys = this.state.selectedKeys;
 
-    this.setState({ selectedKeys }, () => {
-      if (initialSelectedKeys === selectedKeys) {
-        return;
-      }
+      this.setState({ selectedKeys: null }, () => {
+        this.setState({ selectedKeys }, () => {
+          if (_.isEqual(initialSelectedKeys, selectedKeys)) {
+            resolve();
+            return;
+          }
 
-      // console.log('SELECTED KEYS', this.state.selectedKeys);
-      if (
-        this.allKeys.length > 0 &&
-        initialSelectedCount === this.allKeys.length &&
-        this.state.selectedKeys.length === 0
-      ) {
-        // debugger;
-        this.resetDataTypeFilter(true);
-      }
+          // console.log('SELECTED KEYS', this.state.selectedKeys);
+          if (
+            this.allKeys.length > 0 &&
+            initialSelectedCount === this.allKeys.length &&
+            this.state.selectedKeys.length === 0
+          ) {
+            // debugger;
+            this.resetDataTypeFilter(true).then(resolve);
+          } else {
+            // this.resetDataTypeFilter(false);
+            resolve();
+          }
+        });
+      });
     });
   };
 
@@ -234,7 +248,7 @@ const RefinementList = class extends SearchkitComponent {
       <CheckboxItemList
         {...this.props}
         items={this.dataTypeInfo}
-        selectedItems={this.state.selectedKeys}
+        selectedItems={this.state.selectedKeys || []}
         toggleItem={this.toggleItem}
         multiselect={true}
       />
@@ -262,7 +276,7 @@ const RefinementList = class extends SearchkitComponent {
    * @param {string[]} allKeys
    */
   selectedItemsFilter = (allKeys) => {
-    console.log('ALL KEYUS', allKeys);
+    // console.log('ALL KEYUS', allKeys);
     // this.props.selectedItems is of type string[]
     let selectedKeys;
     const selectedCount = this.props.selectedItems.length;
