0591903dfb7b477488dbca196543ac113c62e7de
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-01 15:36:05 +0300

    Improve table block

--- src/components/Blocks/SimpleDataTable/SimpleDataTableView.jsx
+++ src/components/Blocks/SimpleDataTable/SimpleDataTableView.jsx
@@ -8,42 +8,49 @@ const SimpleDataTableView = (props) => {
   const { show_header, description, max_count } = data;
 
   // TODO: sorting
-  const row_size = max_count
-    ? typeof max_count !== 'number'
-      ? parseInt(max_count)
-      : max_count
-    : max_count;
+  const row_size = Math.min(
+    max_count
+      ? typeof max_count !== 'number'
+        ? parseInt(max_count)
+        : max_count
+      : max_count,
+    provider_data && provider_data[Object.keys(provider_data)[0]]?.length,
+  );
 
-  const columns = Object.keys(provider_data);
+  const columns = Object.keys(provider_data || {});
   return (
     <div className="simple-data-table">
       {description ? serializeNodes(description) : ''}
-      <Table>
-        {show_header ? (
-          <Table.Header>
-            <Table.Row>
-              {columns.map((name) => (
-                <Table.HeaderCell key={name}>{name}</Table.HeaderCell>
-              ))}
-            </Table.Row>
-          </Table.Header>
-        ) : (
-          ''
-        )}
-        <Table.Body>
-          {Array(row_size)
-            .fill()
-            .map((_, i) => (
-              <Table.Row key={i}>
-                {Object.keys(provider_data).map((k) => (
-                  <Table.Cell key={`${i}-${k}`}>
-                    {provider_data[k][i]}
-                  </Table.Cell>
+      {row_size ? (
+        <Table>
+          {show_header ? (
+            <Table.Header>
+              <Table.Row>
+                {columns.map((name) => (
+                  <Table.HeaderCell key={name}>{name}</Table.HeaderCell>
                 ))}
               </Table.Row>
-            ))}
-        </Table.Body>
-      </Table>
+            </Table.Header>
+          ) : (
+            ''
+          )}
+          <Table.Body>
+            {Array(row_size)
+              .fill()
+              .map((_, i) => (
+                <Table.Row key={i}>
+                  {Object.keys(provider_data).map((k) => (
+                    <Table.Cell key={`${i}-${k}`}>
+                      {provider_data[k][i]}
+                    </Table.Cell>
+                  ))}
+                </Table.Row>
+              ))}
+          </Table.Body>
+        </Table>
+      ) : (
+        'No results'
+      )}
     </div>
   );
 };
