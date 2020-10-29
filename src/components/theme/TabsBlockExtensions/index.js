6815751098230f304a589087c04b2d37e6265ade
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-29 17:56:28 +0200

    Don't load volto-tabs-block, it's outdated

--- src/components/theme/TabsBlockExtensions/index.js
+++ src/components/theme/TabsBlockExtensions/index.js
@@ -1,24 +1,26 @@
-import TabsTocNavigationView, {
-  tabsTocSchemaExtender,
-} from './TabsTocNavigationView';
-import FlatTabsView from './FlatTabsView';
+export default (config) => config;
 
-export default (config) => {
-  config.blocks.blocksConfig.tabsBlock.extensions = [
-    ...config.blocks.blocksConfig.tabsBlock.extensions,
-    {
-      id: 'tocnav',
-      title: 'TOC Navigation',
-      view: TabsTocNavigationView,
-      schemaExtender: tabsTocSchemaExtender,
-    },
-    {
-      id: 'flatpage',
-      title: 'Flat page',
-      view: FlatTabsView,
-      schemaExtender: null,
-    },
-  ];
-
-  return config;
-};
+// import TabsTocNavigationView, {
+//   tabsTocSchemaExtender,
+// } from './TabsTocNavigationView';
+// import FlatTabsView from './FlatTabsView';
+//
+// export default (config) => {
+//   config.blocks.blocksConfig.tabsBlock.extensions = [
+//     ...config.blocks.blocksConfig.tabsBlock.extensions,
+//     {
+//       id: 'tocnav',
+//       title: 'TOC Navigation',
+//       view: TabsTocNavigationView,
+//       schemaExtender: tabsTocSchemaExtender,
+//     },
+//     {
+//       id: 'flatpage',
+//       title: 'Flat page',
+//       view: FlatTabsView,
+//       schemaExtender: null,
+//     },
+//   ];
+//
+//   return config;
+// };
