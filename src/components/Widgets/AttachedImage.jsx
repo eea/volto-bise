03db4816d54d4cb34b62d751df3a2e8182d9aa5d
Miu Razvan <miu.razvan28@gmail.com>
2021-12-13 13:40:50 +0200

    [Webscore fixe] Removed duplicate condition

--- src/components/Widgets/AttachedImage.jsx
+++ src/components/Widgets/AttachedImage.jsx
@@ -152,7 +152,7 @@ export class UnconnectedAttachedImageWidget extends Component {
               );
             }}
           </Dropzone>
-          {this.state.errorMessage && this.state.errorMessage && (
+          {this.state.errorMessage && (
             <Label basic color="red" pointing>
               {this.state.errorMessage}
             </Label>
